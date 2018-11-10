a2b06d97724d7f1eee234ecd5838525814555e3c
pilartxo <pilarruete@gmail.com>
2018-11-10 22:09:29 +0100

    quitar contrasel

--- includes/conexiones.php
+++ includes/conexiones.php
@@ -1,7 +1,6 @@
 <?php
 
 //$mysqli = mysqli_connect("localhost", "root", "", "Quiz");
-$mysqli = mysqli_connect("db714056396.db.1and1.com", "dbo714056396", "LOSfablos1967_", "db714056396");
 if (!$mysqli) {
     die ("Fallo al conectar a MySQL: " . mysqli_connect_error());
 }
